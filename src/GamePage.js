01eaedd58b07903db30bbb1113b61a1fd874fb67
O. Sharpe <osharpe001@gmail.com>
2023-04-18 13:18:40 -0400

    changed "winner" protocol-better endgame rendering

--- src/GamePage.js
+++ src/GamePage.js
@@ -1,14 +1,17 @@
 // TODO:
-// 7-SETUP A BETTER PUZZLE DESIGN WITH MORE WORDS ON A SINGLE LINE AND
+// 8-SETUP A BETTER PUZZLE DESIGN WITH MORE WORDS ON A SINGLE LINE AND
 //    A SET OF GREEN BACKGROUND BRICKS
-// 8-SET UP COMPUTER PLAYERS BEHAVIOR (POSSIBLE TO USE A USEEFFECT HOOK FOR THIS!)
+// 9-SET UP COMPUTER PLAYERS BEHAVIOR (POSSIBLE TO USE A USEEFFECT HOOK FOR THIS!)
 // ---COMPUTER LETTER SELECTION LIST (IN ORDER OF MOST POPULAR LETTER)
 // -----COMPUTER CHOICE OF LETTER GUESS BASED ON DIFFICULTY LEVEL
 // ---CONDITIONS FOR THE COMPUTER TO BUY A VOWEL(IF CURRENTPLAYER.SCORE>250? COMPUTERLETTERSELECTIONLIST-VOWELS)
-// 9-SET UP WHEEL SO THAT IT STOPS/STARTS ON THE PROPER SEGMENT
-// 10-CHANGE THE WINNER SELECTOR TO A USEEFFECT HOOK TO AVIOD THE ERROR OF RE-RENDERING WHILE GOING TO RESULTS PAGE
+// 10-SET UP WHEEL SO THAT IT STOPS/STARTS ON THE PROPER SEGMENT
 // 11-CREATE A FUNCTION TO LIGHT UP AND THEN TURN THE LETTERS WHEN THEY ARE GUESSED
 // 12-FIX INTERFACE-OPTIONS BOX SO THAT THE BUTTONS DON'T MOVE REGARDLESS OF AMOUNT
+// 13-SET THE "PLAYERNAMES"/"PLAYERS" CONFIGURATION UP TO SETTINGS.JS TO KEEP STABILITY IF THE REFRESH BUTTON IS PRESSED ON THE GAMEPAGE
+// 14-INCLUDE HEADER ON ALL PAGES AND SET THE GAME NAME TO A LINK TO RETURN TO INDEX PAGE
+// ??15??-MAY HAVE TO PUSH THE PUZZLE PHRASE/TYPE SELECTOR UP TO SETTINGS.JS TO CURB APPEAL OF REFRESHING TO CHANGE THE PUZZLE ON GAMEPAGE.JS
+
 
 import { WheelSegments } from "./assets/wheelSegments";
 import Board from "./components/Board";
@@ -42,6 +45,7 @@ export default function GamePage(props) {
   const [attemptToSolve, setAttemptToSolve] = useState(false);
   const [guessPuzzle, setGuessPuzzle] = useState("");
   const [guessPuzzleError, setGuessPuzzleError] = useState("");
+  const [pauseControls, setPauseControls] = useState(false);
   const [players, setPlayers]= useState(playerNames.map(player => (
     {
       "name":player,
@@ -102,29 +106,17 @@ export default function GamePage(props) {
   winner(()=> {
     if (puzzleLetters.every(letter=>guessedLetters.indexOf(letter)>=0)) {
       setStatusMessage(`${currentPlayer.name} HAS WON!!!`);
-      // alert(`${currentPlayer.name} HAS WON!!! (VOWELS)`);
       setTimeout(props.setWinner, 3500, currentPlayer);
       setTimeout(navigate, 3500, "/results");
-    };
+    } else {
+      setPauseControls(false);
+    }
   });
-
-  // const consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
-  // const vowels = ["A", "E", "I", "O", "U"];
-  // const allLetters = [...consonants, ...vowels];
-  // const puzzleLetters = props.puzzlePhrase.split("").filter(letter=>allLetters.indexOf(letter)>-1);
-  // const consonantMultiplier = (puzzleLetters.filter(letter=>letter===latestConsonant)).length;
-  // const vowelMultiplier= (puzzleLetters.filter(letter=>letter===latestVowel)).length;
-  // const wheelValue = wheelInfo[1];
-  // const wheelPrize = wheelInfo[2];
-  // const vowelCost= 250;
-
-  // const currentPlayerNumber = [(turnCount)%(playerNames.length)];
-  // const currentPlayer = players[currentPlayerNumber];
-  // const nextPlayer = players[(turnCount+1)%(playerNames.length)];
 
   const stopSpinning = () => {
     setIsSpinning(false);
-  }
+  };
+
   const spinIt = () =>{
     setStatusMessage("");
     setIsSpinning(!isSpinning);
@@ -150,14 +142,14 @@ export default function GamePage(props) {
       }
     }
     setTimeout(badNews, 5500);
-  }
+  };
 
   const buyVowel = () => {
     let newList=[...players];
     newList[currentPlayerNumber].score-=vowelCost;
     setPlayers(newList);
     setVowelInterface(true);
-  }
+  };
 
   const solveIt = () => {
     // console.log("GAMEPAGE.JS' SOLVEIT IN PROGRESS");
@@ -181,63 +173,6 @@ export default function GamePage(props) {
     };
     setWheelInfo(["", 0, false]);
     setPlayers(newList)
-  };
-
-  const guessLetter = (e) => {
-    e.preventDefault();
-    if (guessedLetters.indexOf(latestConsonant)<0 && latestConsonant!=="") {
-      setGuessedLetters(()=>{
-        const newGuessedLetters=[...guessedLetters];
-        newGuessedLetters.push(latestConsonant);
-        newGuessedLetters.sort();
-        if (consonants.every(consonant=>newGuessedLetters.indexOf(consonant)>=0)) {
-          setNoMoreConsonants(true);
-          setTimeout(setStatusMessage, 3000, "There are no more consonants...");
-        };
-        // if (puzzleLetters.every(letter=>newGuessedLetters.indexOf(letter)>=0)) {
-        //   props.setWinner(currentPlayer);
-        //   // setStatusMessage(`${currentPlayer.name} HAS WON!!!`);
-        //   alert(`${currentPlayer.name} HAS WON!!! (CONSONANTS)`);
-        //   navigate("/results");
-        // };
-        return newGuessedLetters;
-      });
-      if (puzzleLetters.indexOf(latestConsonant)>=0) {
-        changeScore();
-        (consonantMultiplier>1?setStatusMessage(`There are ${consonantMultiplier} ${latestConsonant}'s!`):setStatusMessage(`There is 1 ${latestConsonant}.`));
-      } else {
-        setStatusMessage(`There are no ${latestConsonant}'s. (sorry...)`);
-        changeTurn();
-      }
-      setLatestConsonant("");
-    } else if (guessedLetters.indexOf(latestVowel)<0 && latestVowel!=="") {
-      setGuessedLetters(()=>{
-        const newGuessedLetters=[...guessedLetters];
-        newGuessedLetters.push(latestVowel);
-        newGuessedLetters.sort();
-        if (vowels.every(vowel=>newGuessedLetters.indexOf(vowel)>=0)) {
-          setNoMoreVowels(true);
-          setTimeout(setStatusMessage, 3000, "There are no more vowels...");
-        };
-        // if (puzzleLetters.every(letter=>newGuessedLetters.indexOf(letter)>=0)) {
-        //   props.setWinner(currentPlayer);
-        //   // setStatusMessage(`${currentPlayer.name} HAS WON!!!`);
-        //   alert(`${currentPlayer.name} HAS WON!!! (VOWELS)`);
-        //   navigate("/results");
-        // };
-        return newGuessedLetters;
-      });
-      if (puzzleLetters.indexOf(latestVowel)<0) {
-        setStatusMessage(`There are no ${latestVowel}'s. (sorry...)`);
-        changeTurn();
-      } else if (vowelMultiplier>1){
-        setStatusMessage(`There are ${vowelMultiplier} ${latestVowel}'s!`);
-      } else {
-        setStatusMessage(`There is 1 ${latestVowel}.`);
-      }
-      setVowelInterface(false)
-      setLatestVowel("");
-    }
   };
 
   const handleConsonantGuess = (e) => {
@@ -270,6 +205,53 @@ export default function GamePage(props) {
     };
   };
 
+  const guessLetter = (e) => {
+    e.preventDefault();
+    if (guessedLetters.indexOf(latestConsonant)<0 && latestConsonant!=="") {
+      setGuessedLetters(()=>{
+        const newGuessedLetters=[...guessedLetters];
+        newGuessedLetters.push(latestConsonant);
+        newGuessedLetters.sort();
+        if (consonants.every(consonant=>newGuessedLetters.indexOf(consonant)>=0)) {
+          setNoMoreConsonants(true);
+          setTimeout(setStatusMessage, 3000, "There are no more consonants...");
+        };
+        return newGuessedLetters;
+      });
+      if (puzzleLetters.indexOf(latestConsonant)>=0) {
+        changeScore();
+        (consonantMultiplier>1?setStatusMessage(`There are ${consonantMultiplier} ${latestConsonant}'s!`):setStatusMessage(`There is 1 ${latestConsonant}.`));
+      } else {
+        setStatusMessage(`There are no ${latestConsonant}'s. (sorry...)`);
+        changeTurn();
+      };
+      setPauseControls(true);
+      setLatestConsonant("");
+    } else if (guessedLetters.indexOf(latestVowel)<0 && latestVowel!=="") {
+      setGuessedLetters(()=>{
+        const newGuessedLetters=[...guessedLetters];
+        newGuessedLetters.push(latestVowel);
+        newGuessedLetters.sort();
+        if (vowels.every(vowel=>newGuessedLetters.indexOf(vowel)>=0)) {
+          setNoMoreVowels(true);
+          setTimeout(setStatusMessage, 3000, "There are no more vowels...");
+        };
+        return newGuessedLetters;
+      });
+      if (puzzleLetters.indexOf(latestVowel)<0) {
+        setStatusMessage(`There are no ${latestVowel}'s. (sorry...)`);
+        changeTurn();
+      } else if (vowelMultiplier>1){
+        setStatusMessage(`There are ${vowelMultiplier} ${latestVowel}'s!`);
+      } else {
+        setStatusMessage(`There is 1 ${latestVowel}.`);
+      }
+      setVowelInterface(false);
+      setPauseControls(true);
+      setLatestVowel("");
+    }
+  };
+
   const handlePuzzleGuess = (e) => {
     e.target.value=e.target.value.toUpperCase();
     // console.log("GAMEPAGE.JS HANDLEPUZZLEGUESS' E.TARGET.VALUE: ", e.target.value)
@@ -286,11 +268,9 @@ export default function GamePage(props) {
   const AttemptToSolvePuzzle = (e) => {
     e.preventDefault();
     if (guessPuzzle===props.puzzlePhrase) {
-      // setGuessedLetters([...new Set(guessPuzzle)]);
       setStatusMessage(`Congratulations, ${currentPlayer.name}! That was the correct answer!`)
-      setTimeout(setGuessedLetters, 1500, [...new Set(guessPuzzle)]);
-      // setTimeout(props.setWinner, 3000, currentPlayer);
-      // setTimeout(navigate, 3000, "/results");
+      const filteredGuessPuzzle = [...guessPuzzle].filter(letter => allLetters.indexOf(letter)>=0);
+      setTimeout(setGuessedLetters, 1500, [...new Set(filteredGuessPuzzle)]);
     } else if (guessPuzzle.length>0) {
       setGuessPuzzle("");
       setAttemptToSolve(false);
@@ -337,16 +317,19 @@ export default function GamePage(props) {
         </div>
         <div className="readout">
           <p className="game-status">{statusMessage}</p>
+
           <div className={"interface"}>
             <div className="wheel-info">
-              <p className={wheelValue?null:"hidden"}>Value: {wheelValue}</p>
-              <p className={wheelValue && wheelPrize?null:"hidden"}>Prize: {wheelPrize}</p>
+              <p className={wheelValue?null:"hidden"}>${wheelValue}</p>
+              <p className={wheelValue && wheelPrize?null:"hidden"}>and  a {wheelPrize}</p>
             </div>
+
             <div className="interface-options">
-              <button className={wheelValue || vowelInterface || noMoreConsonants || isSpinning || attemptToSolve?"hidden":"button"} onClick={spinIt} >Spin It!</button>
-              <button className={wheelValue || vowelInterface || isSpinning || attemptToSolve?"hidden":"button"} onClick={solveIt} >Attempt to Solve!</button>
-              <button className={wheelValue || currentPlayer.score<250 || vowelInterface || noMoreVowels || isSpinning || attemptToSolve?"hidden":"button"} onClick={buyVowel} >Buy a Vowel!</button>
+              <button className={wheelValue || vowelInterface || noMoreConsonants || isSpinning || attemptToSolve || pauseControls?"hidden":"button"} onClick={spinIt} >Spin It!</button>
+              <button className={wheelValue || vowelInterface || isSpinning || attemptToSolve || pauseControls?"hidden":"button"} onClick={solveIt} >Attempt to Solve!</button>
+              <button className={wheelValue || currentPlayer.score<250 || vowelInterface || noMoreVowels || isSpinning || attemptToSolve || pauseControls?"hidden":"button"} onClick={buyVowel} >Buy a Vowel!</button>
             </div>
+
             <div className="spin-solve-buy">
               <form className={!wheelValue && !vowelInterface?"hidden":!vowelInterface?"left":"right"}>
                 <label className={!wheelValue?"hidden":null} htmlFor="guess-consonant">Guess a Consonant</label>
@@ -376,7 +359,6 @@ export default function GamePage(props) {
             
               <form className={!attemptToSolve?"hidden":"center"}>
                 <label htmlFor="guess-puzzle">Guess the Puzzle</label>
-                {/* <br/> */}
                 <input
                 ref={solveInput}
                 className={!attemptToSolve?"hidden":null}
@@ -386,11 +368,11 @@ export default function GamePage(props) {
                 placeholder="Guess the Puzzle!"
                 value={guessPuzzle}
                 onChange={handlePuzzleGuess}/>
-                {/* <br/> */}
                 <button htmlFor="guess-puzzle" className={!attemptToSolve?"hidden":"button"} disabled={guessPuzzleDisabled} onClick={AttemptToSolvePuzzle} >Guess the Puzzle!</button>
                 <p className="error-message">{guessPuzzleError}</p>
               </form>
             </div>
+
             <div className="guessed-letters-section">
               {showGuessedLetters?<button onClick={toggleShowGuessedLetters}>≤</button>:<button onClick={toggleShowGuessedLetters}>≥</button>}
               <p className={showGuessedLetters?null:"hidden"}>Guessed Letters:</p>
