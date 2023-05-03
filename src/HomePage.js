2e97638ee13cc5e58a22e930e11547e2a3034951
O. Sharpe <osharpe001@gmail.com>
2023-05-03 19:42:56 -0400

    small clean up after tests

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,12 @@
 import Wheel from "./components/Wheel.js"
 
 export default function HomePage() {
+
   return (
+
     <div className="home-page">
         <h1>Spin the wheel and climb the Letter to Success!</h1>
         <Wheel />
     </div>
-  )
-}
+  );
+};
