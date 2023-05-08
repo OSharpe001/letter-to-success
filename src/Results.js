ba2929e092861ef22417115197052a7b439c4b50
O. Sharpe <osharpe001@gmail.com>
2023-05-07 20:07:21 -0400

    silencing the entire game and stopping console.log

--- src/Results.js
+++ src/Results.js
@@ -1,35 +1,35 @@
 import { useNavigate } from "react-router-dom";
-import cheering_kids from "./assets/sounds/cheering_kids.m4a";
-import cheering_crowd from "./assets/sounds/cheering_crowd.m4a";
-import aww from "./assets/sounds/aww.m4a";
-import { useState, useEffect } from "react";
+// import cheering_kids from "./assets/sounds/cheering_kids.m4a";
+// import cheering_crowd from "./assets/sounds/cheering_crowd.m4a";
+// import aww from "./assets/sounds/aww.m4a";
+// import { useState, useEffect } from "react";
 
 export default function Results(props) {
 
-  const [winner, setWinner] = useState(props.winner.name);
+  // const [winner, setWinner] = useState(props.winner.name);
   const navigate = useNavigate();
-  const cheerForMe = useEffect;
-  const cheer1 = new Audio(cheering_kids);
-  const cheer2 = new Audio(cheering_crowd);
-  const sad = new Audio(aww);
+  // const cheerForMe = useEffect;
+  // const cheer1 = new Audio(cheering_kids);
+  // const cheer2 = new Audio(cheering_crowd);
+  // const sad = new Audio(aww);
 
-  cheerForMe(() => {
-    if (winner.indexOf("Computer")<0) {
-        const randomNum = Math.random();
-        randomNum>.5?cheer2.play():cheer1.play();
-      } else {
-        sad.play();
-      }
-  }, [winner]);
+  // cheerForMe(() => {
+  //   if (winner.indexOf("Computer")<0) {
+  //       const randomNum = Math.random();
+  //       randomNum>.5?cheer2.play():cheer1.play();
+  //     } else {
+  //       sad.play();
+  //     }
+  // }, [winner]);
 
   const playAgain = () => {
-    setWinner("");
+    // setWinner("");
     props.setWinner("");
     navigate("/settings");
   };
 
   const backHome = () => {
-    setWinner("");
+    // setWinner("");
     props.setWinner("");
     navigate("/");
   };
