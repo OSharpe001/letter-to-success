5f3e6de0a33045a55fe6ea0da76025b86e73efdd
O. Sharpe <osharpe001@gmail.com>
2023-11-07 23:36:47 -0500

    changed the portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
 
     <footer>
       <nav>
-        <Link aria-label="On Click" to="https://osharpesportfolio.vercel.app" className="nav-item button">O. Sharpe's Portfolio</Link>
+        <Link aria-label="On Click" to="https://osharpesportfolio.netlify.app" className="nav-item button">O. Sharpe's Portfolio</Link>
         </nav>
     </footer>
   );
