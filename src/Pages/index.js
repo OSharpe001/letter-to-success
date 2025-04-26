4ec8c31942b8768949a9ce9262716196788e1f8b
O. Sharpe <osharpe001@gmail.com>
2025-04-26 15:18:47 -0400

    creation of index files for succinct imports

---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+export { default as GamePage } from "./GamePage";
+export { default as HomePage } from "./HomePage";
+export { default as Results } from "./Results";
+export { default as Settings } from "./Settings";
---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+export { default as climbing_icon_2 } from "./climbing_icon_2.png";
+export { default as climbing_icon_3 } from "./climbing_icon_3.png";
+export { default as climbing_icon } from "./climbing_icon.png";
+export { default as doubleLeftArrow } from "./doubleLeftArrow.png";
+export { default as doubleRightArrow } from "./doubleRightArrow.png";
+export { default as single_left_arrow } from "./single_left_arrow.png";
+export { default as single_right_arrow } from "./single_right_arrow.png";
+export { default as whiteMuteSpeaker } from "./whiteMuteSpeaker.png";
+export { default as whiteSpeaker } from "./whiteSpeaker.png";
