cd4a86c37d6c333bed4ade065a45b152b33e7cc4
O. Sharpe <osharpe001@gmail.com>
2025-05-05 19:56:48 -0400

    added Footer component to page and changed class name

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,12 @@
-import { Wheel } from "../components";
+import { Wheel, Footer } from "../components";
 
 export default function HomePage() {
 
   return (
-
-    <div className="home-page">
-        <h1>Spin the wheel and climb the Letter to Success!</h1>
-        <Wheel />
+    <div className="homePage">
+      <h1>Spin the wheel and climb the Letter to Success!</h1>
+      <Wheel />
+      <Footer />
     </div>
   );
 };
