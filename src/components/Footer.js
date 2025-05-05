7f1a25d4d022971680caa7ac53768b1ee1182b64
O. Sharpe <osharpe001@gmail.com>
2025-05-05 13:00:34 -0400

    slight cleanup of Footer file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 export default function Footer() {
 
   return (
-
     <footer>
       <nav>
         <Link aria-label="On Click" target="_blank" to="https://osharpesportfolio.netlify.app" className="nav-item button">O. Sharpe's Portfolio</Link>
-        </nav>
+      </nav>
     </footer>
   );
 };
