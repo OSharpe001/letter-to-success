cf1a06d5d532863c8c80b995c87da128c45c2d0d
O. Sharpe <osharpe001@gmail.com>
2025-05-08 01:59:46 -0400

    fixing ResultsPage footer and changing classname

--- src/Pages/Results.js
+++ src/Pages/Results.js
@@ -17,47 +17,49 @@ export default function Results({ setWinner, winner }) {
   };
 
   return (
-    <div className="results-page">
-      {winner.name.indexOf("Computer")<0?
+    <div className="resultsPage">
+      {winner.name.indexOf("Computer") < 0 ?
         <>
-          <br/>
+          <br />
           <h1>Congratulations, {winner.name}!</h1>
-          <br/>
+          <br />
           <h2>You have won ${winner.score}</h2>
-          <br/>
-          {winner.prizes.length?
+          <br />
+          {winner.prizes.length ?
             <>
               <h2>and these are your prizes:</h2>
-              <br/>
+              <br />
               {winner.prizes.map((prize, index) => <h3 key={index} className="winner's_prizes">- A {prize}</h3>)}
             </>
             :
-            <><h3>Sorry. You didn't win any prizes, this time.</h3><br/></>
+            <><h3>Sorry. You didn't win any prizes, this time.</h3><br /></>
           }
         </>
         :
         <div className="loss">
           <h1><strong>Sorry, humans. You've Lost!</strong></h1>
-        <br/>
+          <br />
           <h1>{winner.name} has won ${winner.score}!</h1>
-          <br/>
-          {winner.prizes.length?
+          <br />
+          {winner.prizes.length ?
             <>
               <h2>{winner.name}'s  prizes:</h2>
-              <br/>
+              <br />
               {winner.prizes.map((prize, index) => <h3 key={index} className="winner's_prizes">- A {prize}</h3>)}
             </>
             :
-            <><h3>{winner.name} didn't win any prizes, this time.</h3><br/></>
+            <><h3>{winner.name} didn't win any prizes, this time.</h3><br /></>
           }
         </div>
       }
-          <br/>
-          <button className="button" onClick={playAgain}>Play Again!</button>
-          <br/>
-          <br/>
-          <button className="button" onClick={backHome}>Back to Home</button>
-          <Footer />
+      <div>
+        <br />
+        <button className="button" onClick={playAgain}>Play Again!</button>
+
+        <br />
+        <button className="button" onClick={backHome}>Back to Home</button>
+      </div>
+      <Footer />
     </div>
   );
 };
