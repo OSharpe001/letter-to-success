cf336f70491bf5a2dcfb324bf23aa554c0ae7e0d
O. Sharpe <osharpe001@gmail.com>
2025-05-17 18:27:19 -0400

    dismissed Wheel and added video. Also created small function for video operation and muting

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,22 @@
-import { Wheel, Footer } from "../components";
+import { Footer } from "../components";
+import { useState } from "react";
+import vid from "../assets/video/LetterToSuccessHomePage.mp4";
 
-export default function HomePage() {
+export default function HomePage({ sound }) {
+
+  const [videoIsPlaying, setVideoIsPlaying] = useState(false);
+
+  const playVid = () => {
+    setVideoIsPlaying(!videoIsPlaying);
+    setTimeout(setVideoIsPlaying, 100, (videoIsPlaying));
+  };
 
   return (
-    <div className="homePage">
+    <div className="homePage" onClick={playVid}>
       <h1>Spin the wheel and climb the Letter to Success!</h1>
-      <Wheel />
+      <video className="vid" width="73%" autoPlay muted={videoIsPlaying || !sound}>
+        <source src={vid} type="video/mp4" />
+      </video>
       <Footer />
     </div>
   );
