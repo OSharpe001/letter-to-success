5fe9aeffa0973a0337efd2743f8b98bb52861978
O. Sharpe <osharpe001@gmail.com>
2025-05-27 16:46:54 -0400

    addition of new images into the index

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -6,3 +6,13 @@ export { default as single_left_arrow } from "./single_left_arrow.png";
 export { default as single_right_arrow } from "./single_right_arrow.png";
 export { default as whiteMuteSpeaker } from "./whiteMuteSpeaker.png";
 export { default as whiteSpeaker } from "./whiteSpeaker.png";
+
+export { default as cloud } from "./cloud2.png";
+export { default as islandNight } from "./islandNight3.png";
+export { default as newCar1 } from "./newCar1.png";
+export { default as newCar2 } from "./newCar2.png";
+export { default as newCar3 } from "./newCar3.png";
+export { default as newCar4 } from "./newCar4.png";
+export { default as newCar5 } from "./newCar5.png";
+export { default as nightPlane } from "./nightPlane.png";
+export { default as suburbanStreet } from "./suburbanStreet.png";
